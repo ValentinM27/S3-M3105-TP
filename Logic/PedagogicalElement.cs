2df548bc61fe2f0b1496f6e8f017e8c55ecb46b6
ValentinM27 <marguerie12@gmail.com>
2021-09-23 11:10:21 +0200

    Création des classes de Logic

---
+++ Logic/PedagogicalElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// Classe gérant les matières
+    /// </summary>
+    public class PedagogicalElement
+    {
+        //Nom de la matière
+        private String name;
+
+        public String Name
+        {
+            get => this.name;
+
+            set
+            {
+                if (value == null || value == "") throw new Exception("The name must be entered");
+                this.name = value;
+            }
+        }
+
+        //Coeficient de la matière
+        private float coef;
+
+        public float Coef
+        {
+            get => this.coef;
+
+            set
+            {
+                if (value <= 0) throw new Exception("The coef must be >0");
+                this.coef = value;
+            }
+
+        }
+    }
+}
