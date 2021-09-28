69fe781b57c3ccdc962c6c1258ac1c917ff5b98d
ValentinM27 <74249732+ValentinM27@users.noreply.github.com>
2021-09-28 08:57:13 +0200

    Test

---
+++ NoteBook/EditElementWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Logic;
+
+namespace NoteBook
+{
+    /// <summary>
+    /// Logique d'interaction pour EditElementWindow.xaml
+    /// </summary>
+    public partial class EditElementWindow : Window
+    {
+        public EditElementWindow(PedagogicalElement elt)
+        {
+            InitializeComponent();
+            this.element = elt;
+        }
+
+        private Ped
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Validate(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
