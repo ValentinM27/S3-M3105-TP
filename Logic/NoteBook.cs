c2344cb46ea7a66143c828dc59c6aaeb6af06191
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2021-10-14 10:25:14 +0200

    Fin Partie 2 TP n°4

--- Logic/NoteBook.cs
+++ Logic/NoteBook.cs
@@ -60,6 +60,31 @@ namespace Logic
         {
             this.units.Remove(unit);
         }
+
+        /// <summary>
+        /// Retroune la liste des modules peut importe l'unité
+        /// </summary>
+        /// <returns></returns>
+        public Module[] ListModules()
+        {
+            List<Module> res = new List<Module>();
+
+            foreach(Unit u in this.units)
+            {
+                res.AddRange(u.ListModules());
+            }
+
+            return res.ToArray();
+        }
+
+        /// <summary>
+        /// Permet d'ajouter un examen à la liste des examens
+        /// </summary>
+        /// <param name="e"></param>
+        public void AddExam(Exam e)
+        {
+            this.exams.Add(e);
+        }
     }
 }
  
--- TestLogic/TestNotebook.cs
+++ TestLogic/TestNotebook.cs
@@ -74,5 +74,40 @@ namespace TestLogic
             //On test la suppression de l'unit 
             Assert.Equal(emptyRes, notebook.ListUnits());
         }
+
+        //Test la fonction ListModule 
+        [Fact]
+        public void TetsListModule()
+        {
+            Logic.NoteBook notebook = new NoteBook();
+            Logic.Unit unit = new Unit();
+
+            unit.Name = "UE1";
+            unit.Coef = 8;
+
+            Logic.Module module = new Module();
+            module.Name = "Programmation";
+            module.Coef = 10;   
+
+            unit.Add(module);
+
+            notebook.AddUnit(unit);
+
+            Module[] test = unit.ListModules();
+            
+            Assert.Equal(test, notebook.ListModules());
+
+        }
+
+        //Test la fonction AddExam
+        [Fact]
+        public void TestAddExam()
+        {
+            Logic.NoteBook notebook = new NoteBook();
+            Logic.Exam exam = new Exam();
+
+            notebook.AddExam(exam);
+            Assert.Single(notebook.Exams);
+        }
     }
 }
