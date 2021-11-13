c33d5088780657917ced7217ddf09a3d8a5bc062
ValentinM27 <74249732+ValentinM27@users.noreply.github.com>
2021-11-13 09:53:32 +0100

    Interface de stockage

---
+++ Logic/IStorage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    /// <summary>
+    /// Interface dédiée au stockage des notebook
+    /// </summary>
+    public interface IStorage
+    {
+        /// <summary>
+        /// Permet de charger un notebook
+        /// </summary>
+        /// <returns></returns>
+        Logic.NoteBook Load();
+
+        /// <summary>
+        /// Permet de sauvegarder un notebook
+        /// </summary>
+        /// <param name="nb"></param>
+        /// <returns></returns>
+        Logic.NoteBook Save(Logic.NoteBook nb)
+;    }
+}
